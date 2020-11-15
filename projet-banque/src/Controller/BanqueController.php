6e06d9e2271093302a9d86543af75cd883df886c
Ulysse Perret <ulysseperret@orange.fr>
2020-11-15 16:07:06 +0100

    ajout annotation 2

--- projet-banque/src/Controller/BanqueController.php
+++ projet-banque/src/Controller/BanqueController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class BanqueController
+class BanqueController extends AbstractController
 {
     /**
     * @Route("/banque")
@@ -14,10 +15,9 @@ class BanqueController
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>
-            Je suis une banque </br>
-            Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('banque/banque.html.twig', [
+            'number' => $number,
+        ]);
+
     }
 }
