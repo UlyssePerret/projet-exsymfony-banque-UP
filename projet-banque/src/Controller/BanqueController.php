84cbcdf22844c4c4c02c7afe3af2d50cfcc74ec1
Ulysse Perret <ulysseperret@orange.fr>
2020-11-15 15:41:16 +0100

    ajout premier page banque

---
+++ projet-banque/src/Controller/BanqueController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class BanqueController
+{
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>
+            Je suis une banque </br>
+            Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
